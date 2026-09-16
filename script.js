78e9d031823aedaf077f7688416e2aa9384e8239
majulyah3121-commits <majulyah3121@gmail.com>
2026-09-16 13:22:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const products = [
         name: "Carnet d'Organisation Douceur",
         category: "Organisation",
         price: 18.50,
-        image:"./images/20260821_164820.jpg",
+        image:"https://raw.githubusercontent.com/majulyah3121-commits/majulyah-site/main/images/20260821_164820.jpg",
         description: "Un magnifique carnet couverture rigide en lin couleur sauge. 160 pages de papier crémeux haut de gamme idéal pour organiser vos semaines et déposer vos pensées.",
         variants: ["Lin Sauge", "Terracotta Charnelle", "Beige Sablé"]
     },
