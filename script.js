0954da275244c3f921e1e0e4585252453e5d1644
majulyah3121-commits <majulyah3121@gmail.com>
2026-09-16 15:46:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ const products = [
   name: "Stickers 6ème",
   category: "Organisation",
   price: 12.00,
-  image: "https://raw.githubusercontent.com/majulyah3121-commits/majulyah-site/main/images/20260821_164820.jpg",   
+  image: "https://raw.githubusercontent.com/majulyah3121-commits/majulyah-site/main/images/20260821_164820.jpg",
+  description: "Des stickers personnalisés pour la rentrée en 6ème", 
   colors: [
     "Bleu foncé", "Bleu clair", "Bleu canard", "Vert clair",
     "Vert foncé", "Orange", "Rouge clair", "Rouge foncé",
@@ -23,7 +24,7 @@ const products = [
   category: "Organisation",
   price: 10.00,
   image: "https://raw.githubusercontent.com/majulyah3121-commits/majulyah-site/main/images/20260913_154315.jpg",
-  description: "Des stickers personnalisés pour que chaque jouet trouve sa place ! 🧸✨",
+  description: "Des stickers personnalisés pour que chaque jouet trouve sa place",
   variants: [
     "Poupée", "Bébé", "Barbie", "Dînette", "Princesse", "Déguisement",
     "Coiffure", "Peluche", "Super-héros", "Dinosaure", "Bricolage",
